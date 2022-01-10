839771f20e90a86a05ad098321a64c9fa3834f5f
devfel <devfelizardo@gmail.com>
2022-01-09 22:52:39 -0500

    removing comments

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,14 +1,4 @@
 import knex from "knex";
-import path from 'path';
-
-// //USING SQLITE3
-// const db = knex({
-//     client: 'sqlite3',
-//     connection: {
-//         filename: path.resolve(__dirname, 'database.sqlite')
-//     },
-//     useNullAsDefault: true,
-// });
 
 //USING POSTGRES
 const db = knex({
@@ -16,15 +6,6 @@ const db = knex({
 
     //PRODUCTION
     connection: process.env.DATABASE_URL
-
-    // //DEV
-    // connection: {
-    //     host: 'localhost',
-    //     port: 5432,
-    //     user: 'postgres',
-    //     password: 'docker',
-    //     database: 'forage_trucks_postgresdb'
-    // }
 });
 
 
--- src/knexfile.ts
+++ src/knexfile.ts
@@ -1,36 +1,9 @@
-//import path from 'path';
-
-// //USING SQLITE3
-// module.exports = {
-//     client: 'sqlite3',
-//     connection: {
-//         filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite')
-//     },
-//     migrations: {
-//         directory: path.resolve(__dirname, 'src', 'database', 'migrations')
-//     },
-//     useNullAsDefault: true,
-// };
-
 //USING POSTGRES
 module.exports = {
     client: 'postgres',
 
     //PRODUCTION
     connection: process.env.DATABASE_URL,
-
-    // //DEV
-    // connection: {
-    //     host: 'localhost',
-    //     port: 5432,
-    //     user: 'postgres',
-    //     password: 'docker',
-    //     database: 'forage_trucks_postgresdb'
-    // },
-
-    // migrations: {
-    //     directory: path.resolve(__dirname, 'src', 'database', 'migrations')
-    // }
 
     migrations: {
         directory: __dirname + '/src/database/migrations'
