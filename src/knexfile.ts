3d08b549cb127373ad6c3d6339c781fe6c575d15
devfel <devfelizardo@gmail.com>
2024-10-31 21:05:28 -0300

    Adding env file example and fixing config to use the env variables

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -14,21 +14,6 @@
 //     }
 // };
 
-// //DEVELOPMENT
-// module.exports = {
-//   client: "postgres",
-//   connection: {
-//     host: "localhost",
-//     port: 5432,
-//     user: "postgres",
-//     password: "docker", // replace with the actual password
-//     database: "forage_trucks_postgresdb",
-//   },
-//   migrations: {
-//     directory: __dirname + "/database/migrations",
-//   },
-// };
-
 module.exports = {
   client: "postgres",
   connection: process.env.DATABASE_URL,
--- src/server.ts
+++ src/server.ts
@@ -12,5 +12,5 @@ app.use(routes);
 // localhost:3000
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server Running on the Port: ${port}`);
+  console.log(`Server Running on port: ${port}`);
 });
